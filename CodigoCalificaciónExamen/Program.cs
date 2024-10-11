b2666d7ab6f883a979b02f1ad95474df6b7f741f
Yunkei <alva_alcantar24@unikino.edu.mx>
2024-10-11 17:21:25 -0600

    Agregar archivos de proyecto.

---
+++ CodigoCalificaciónExamen/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CodigoCalificaciónExamen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa la calificación:");
+            
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero >= 70)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Calificación aprobatoria.");
+            }
+            else if (numero >= 50 && numero <= 70)
+            {
+                Console.ForegroundColor= ConsoleColor.Red;    
+                Console.WriteLine("Calificación reprobatoria con derecho a regularizar.");
+            }
+            else if(numero < 50)
+            { 
+                Console.ForegroundColor= ConsoleColor.Red;
+                Console.WriteLine("Reprobado, debe volver a cursar.");
+            }
+
+        }
+    }
+}
